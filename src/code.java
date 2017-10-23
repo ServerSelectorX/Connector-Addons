bcb3c6beb2d05d8deb4cd11350a2eb42c5385992
Derkades <robinslot@hotmail.nl>
2017-10-23 11:32:19 +0200

    Code and info.yml

--- src/code.java
+++ src/code.java
@@ -1,4 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class code {
+import org.bukkit.Bukkit;
+
+import xyz.derkades.serverselectorx.connector.AddonClass;
+
+public class code implements AddonClass {
+
+	@Override
+	public Map<String, String> getPlaceholders() {
+		Map<String, String> map = new HashMap<>();
+		map.put("online", Bukkit.getOnlinePlayers().size() + "");
+		map.put("max", Bukkit.getMaxPlayers() + "");
+		return map;
+	}
+
+	@Override
+	public Map<String, String> getConfigDefaults() {
+		return null;
+	}
 
 }
