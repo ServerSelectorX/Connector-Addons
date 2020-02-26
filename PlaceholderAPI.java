6c3a7e748c51a8319844be37402000154fcb06f1
Derkades <robinslot@hotmail.nl>
2020-02-26 20:02:14 +0100

    PlaceholderAPI - Add null checking

--- PlaceholderAPI.java
+++ PlaceholderAPI.java
@@ -22,20 +22,32 @@ public class PlaceholderAPI extends Addon {
 
 	@Override
 	public String getVersion() {
-		return "1.1.0";
+		return "1.1.1";
 	}
 
 	@Override
 	public void onLoad() {
-		final ConfigurationSection global = this.config.getConfigurationSection("global");
-		final ConfigurationSection player = this.config.getConfigurationSection("player");
-
-		for (final String key : global.getKeys(false)) {
-			addPlaceholder(key, () -> me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(null, "%" + global.getString(key) + "%"));
+		if (config == null) {
+			System.err.println("Config was not loaded");
+			return;
 		}
-
-		for (final String key : player.getKeys(false)) {
-			addPlayerPlaceholder(key, (uuid, name) -> me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(uuid), "%" + player.getString(key) + "%"));
+		
+		if (config.isConfigurationSection("global")) {
+			final ConfigurationSection global = this.config.getConfigurationSection("global");
+			for (final String key : global.getKeys(false)) {
+				addPlaceholder(key, () -> me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(null, "%" + global.getString(key) + "%"));
+			}
+		} else {
+			System.err.println("Global placeholders configuration section missing");
+		}
+		
+		if (config.isConfigurationSection("player")) {
+			final ConfigurationSection player = this.config.getConfigurationSection("player");
+			for (final String key : player.getKeys(false)) {
+				addPlayerPlaceholder(key, (uuid, name) -> me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(uuid), "%" + player.getString(key) + "%"));
+			}
+		} else {
+			System.err.println("Global placeholders configuration section missing");
 		}
 	}
 
