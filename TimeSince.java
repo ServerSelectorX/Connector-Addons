8b005b2211f7792f103d0ca00c2dd3dd0ffd9871
Robin <robinslot@hotmail.nl>
2022-05-26 10:29:33 +0200

    Fix uptime

--- TimeSince.java
+++ TimeSince.java
@@ -21,7 +21,7 @@ public class TimeSince extends Addon {
 
 	@Override
 	public String getVersion() {
-		return "1.1.1";
+		return "1.1.2";
 	}
 
 	@Override
@@ -30,14 +30,14 @@ public class TimeSince extends Addon {
 
 		for (final String placeholderName : this.config.getConfigurationSection("placeholders").getKeys(false)) {
 			addPlaceholder(placeholderName, () -> {
-				long diff;
+				long diff; // In seconds
 				if (this.config.isLong("placeholders." + placeholderName) || this.config.isInt("placeholders." + placeholderName)) {
 					final long timestamp = this.config.getLong("placeholders." + placeholderName);
-					diff = System.currentTimeMillis() / 1000 - timestamp; //timestamp is in seconds
+					diff = System.currentTimeMillis() / 1000 - timestamp;
 				} else {
 					final String type = this.config.getString("placeholders." + placeholderName);
 					if (type.equals("uptime")) {
-						diff = (System.currentTimeMillis() - ManagementFactory.getRuntimeMXBean().getUptime()) / 1000;
+						diff = ManagementFactory.getRuntimeMXBean().getUptime() / 1000;
 					} else {
 						return "error: invalid value";
 					}
